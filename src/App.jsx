234e85ba776d0a0cce8b072cae0054375dc384db
Ricky K <riickyy_kvarn@hotmail.se>
2023-03-01 13:06:45 +0100

    color-test

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from "react";
+import Colors from "./Colors";
+
+export const ColorsContext = createContext(null);
+
+export default function App() {
+  const [colors, setColors] = useState([
+    "#fff",
+    "#fff",
+    "#fff",
+    "#fff",
+    "#ddd",
+  ]);
+
+  return (
+    <ColorsContext.Provider value={{ colors: colors, setColors: setColors }}>
+      <Colors />
+    </ColorsContext.Provider>
+  );
+}
